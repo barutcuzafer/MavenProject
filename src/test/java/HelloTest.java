359fcea6963b82f0eaede8f9dc522673091e6429
barutcuzafer <barutcuzafer1@gmail.com>
2021-12-16 12:52:50 +0300

    update more hello feature

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,6 +2,8 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello Test");
         System.out.println("Developing Hello");
+        System.out.println("Local github repo is already published");
+        System.out.println("doing");
     }
 
 }
